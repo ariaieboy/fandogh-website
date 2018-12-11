d8f283d688cb89e04fd373d512df8ae9ecfbdc5e
davodaslanifakor <davodaslanifakor@gmail.com>
2018-12-11 14:57:26 +0330

    fix image and version uploade

--- pages/dashboard/images/_image/versions/create.vue
+++ pages/dashboard/images/_image/versions/create.vue
@@ -1,92 +1,121 @@
 <template>
   <div class="row">
-    <div class="col-md-6 col-xs-12" >
+    <div class="col-md-6 col-xs-12">
       <h2>آپلود ورژن</h2>
       <div class="fandogh-form-group">
-        <f-input v-model="name"  styles="input-white input-block input-dashboard input-disable" placeholder="نام ایمیج را در این قسمت بنویسید"> </f-input>
+        <f-input
+          v-model="name"
+          styles="input-white input-block input-dashboard input-disable"
+          placeholder="نام ایمیج را در این قسمت بنویسید"
+        ></f-input>
       </div>
       <div class="fandogh-form-group">
-        <f-input v-model="version" styles="input-white input-block input-dashboard" placeholder="ورژن ایمیج را در این قسمت بنویسید"> </f-input>
+        <f-input
+          v-model="version"
+          styles="input-white input-block input-dashboard"
+          placeholder="ورژن ایمیج را در این قسمت بنویسید"
+        ></f-input>
       </div>
       <div class="fandogh-form-group margin-40">
-        <file id="source" />
+        <file id="source"/>
       </div>
       <div class="fandogh-form-group margin-top-100">
-        <f-button @onClick="createImageVersion" styles="red block" v-if="!loading"  > اتمام ساخت </f-button>
-        <progress-bar v-if="loading" :progress="progress"> </progress-bar>
+        <f-button @onClick="createImageVersion" styles="red block" v-if="!loading">اتمام ساخت</f-button>
+        <progress-bar v-if="loading" :progress="progress"></progress-bar>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import FInput from "~/components/elements/input";
+import FButton from "~/components/elements/button";
+import File from "~/components/elements/file";
+import { formData } from "~/utils/formData";
+import ProgressBar from "~/components/Dashboard/progress-bar";
+import ErrorReporter from "~/utils/ErrorReporter";
+import FormValidator from "~/utils/formValidator";
 
-  import FInput from '~/components/elements/input'
-  import FButton from '~/components/elements/button'
-  import File from '~/components/elements/file'
-  import {formData} from "~/utils/formData";
-  import ProgressBar from '~/components/Dashboard/progress-bar'
-  import ErrorReporter from '~/utils/ErrorReporter'
-  import FormValidator from '~/utils/formValidator'
-
-  export default {
-    layout: 'dashboard',
-    data(){
-      return {
-        name: this.$route.params.image,
-        version: '',
-        loading: false,
+export default {
+  layout: "dashboard",
+  data() {
+    return {
+      name: this.$route.params.image,
+      version: "",
+      loading: false,
+      source: null
+    };
+  },
+  computed: {
+    progress() {
+      return this.$store.state.progress;
+    }
+  },
+  mounted() {
+    var vm = this;
+    let btnfile = document.querySelector("#source");
+    btnfile.addEventListener("change", function(e) {
+      var files = e.target.files[0];
+      if (!files) {
+        vm.source = null;
+        return;
       }
-    },
-    computed:{
-      progress(){
-        return this.$store.state.progress
-      }
-    },
-    components:{
-      FInput,
-      FButton,
-      File,
-      ProgressBar
-    },
-    methods:{
-      createImageVersion(){
+      vm.source = files;
+    });
+  },
+  components: {
+    FInput,
+    FButton,
+    File,
+    ProgressBar
+  },
+  methods: {
+    createImageVersion() {
+      if (!FormValidator(this.$data, { version: { required: true } }))
+        return false;
+      if (this.version.length && this.source) {
+        this.loading = true;
 
-        if(!FormValidator(this.$data, {version: {required: true}})) return false
-
-        this.loading = true
-        if(this.version.length){
-          this.loading = true
-
-          let fd = formData([
-            {
-              name: 'version',
-              value: this.version
-            },
-            {
-              type: 'image',
-              name: 'source'
-            }
-          ])
-          this.$store.dispatch('createImageVersion', {name: this.name, formData: fd}).then(res => {
+        let fd = formData([
+          {
+            name: "version",
+            value: this.version
+          },
+          {
+            type: "image",
+            name: "source"
+          }
+        ]);
+        this.$store
+          .dispatch("createImageVersion", { name: this.name, formData: fd })
+          .then(res => {
             this.$notify({
               title: res.message,
               time: 4000,
-              type: 'success'
-            })
-            this.$router.push(`/dashboard/images/${this.name}/versions/${this.version}/logs`)
-          }).catch(e => {
-            this.loading = false
+              type: "success"
+            });
+            this.$router.push(
+              `/dashboard/images/${this.name}/versions/${this.version}/logs`
+            );
+          })
+          .catch(e => {
+            this.loading = false;
             ErrorReporter(e, this.$data, true).forEach(error => {
               this.$notify({
                 title: error,
                 time: 5000,
-                type: 'error'
-              })
-            })
-          })
-        }
+                type: "error"
+              });
+            });
+          });
+      }else{
+         this.$notify({
+              title: 'شما فایلی آپلود نکرده اید',
+              time: 4000,
+              type: "error"
+            });
       }
     }
   }
+};
 </script>
--- pages/dashboard/images/create.vue
+++ pages/dashboard/images/create.vue
@@ -1,121 +1,158 @@
 <template>
   <div class="row">
-    <div class="col-md-6 col-xs-12" >
+    <div class="col-md-6 col-xs-12">
       <h2>ساخت ایمیج</h2>
       <div class="fandogh-form-group">
         <label>نام ایمیج</label>
-        <f-input name="name" v-model="name" styles="input-white input-block input-dashboard" placeholder="نام ایمیج را در این قسمت بنویسید"> </f-input>
+        <f-input
+          name="name"
+          v-model="name"
+          styles="input-white input-block input-dashboard"
+          placeholder="نام ایمیج را در این قسمت بنویسید"
+        ></f-input>
       </div>
       <div class="fandogh-form-group">
         <label>ورژن ایمیج</label>
-        <f-input name="version" v-model="version" class="version" styles="input-white input-block input-dashboard" placeholder="ورژن ایمیج را در این قسمت بنویسید"> </f-input>
+        <f-input
+          name="version"
+          v-model="version"
+          class="version"
+          styles="input-white input-block input-dashboard"
+          placeholder="ورژن ایمیج را در این قسمت بنویسید"
+        ></f-input>
       </div>
       <div class="fandogh-form-group margin-40">
-        <file class="file" id="source" />
+        <file class="file" id="source"/>
       </div>
       <div class="fandogh-form-group margin-top-100">
-        <f-button v-if="!loading" @onClick="createImage" styles="red block"  > اتمام ساخت </f-button>
-        <f-button  v-if="loading && !loadingProgress"  styles="red block"  > در حال ساخت </f-button>
-        <progress-bar v-if="loadingProgress" :progress="progress"> </progress-bar>
+        <f-button v-if="!loading" @onClick="createImage" styles="red block">اتمام ساخت</f-button>
+        <f-button v-if="loading && !loadingProgress" styles="red block">در حال ساخت</f-button>
+        <progress-bar v-if="loadingProgress" :progress="progress"></progress-bar>
       </div>
-
     </div>
   </div>
 </template>
 
 <script>
-  import FInput from '~/components/elements/input'
-  import FButton from '~/components/elements/button'
-  import File from '~/components/elements/file'
-  import {formData} from "~/utils/formData";
-  import ProgressBar from '~/components/Dashboard/progress-bar'
-  import ErrorReporter from '~/utils/ErrorReporter'
-  import FormValidator from '~/utils/formValidator'
+import FInput from "~/components/elements/input";
+import FButton from "~/components/elements/button";
+import File from "~/components/elements/file";
+import { formData } from "~/utils/formData";
+import ProgressBar from "~/components/Dashboard/progress-bar";
+import ErrorReporter from "~/utils/ErrorReporter";
+import FormValidator from "~/utils/formValidator";
 
-  export default {
-    layout: 'dashboard',
-    data(){
-      return {
-        name: '',
-        version: '',
-        loading: false,
-        loadingProgress: false
+export default {
+  layout: "dashboard",
+  data() {
+    return {
+      name: "",
+      version: "",
+      loading: false,
+      loadingProgress: false,
+      source: ""
+    };
+  },
+  computed: {
+    progress() {
+      return this.$store.state.progress;
+    }
+  },
+  components: {
+    FInput,
+    FButton,
+    File,
+    ProgressBar
+  },
+  mounted() {
+    var vm = this;
+    let btnfile = document.querySelector("#source");
+    btnfile.addEventListener("change", function(e) {
+      var files = e.target.files[0];
+      if (!files) {
+        vm.source = null;
+        return;
       }
-    },
-    computed:{
-      progress(){
-        return this.$store.state.progress
-      }
-    },
-    components:{
-      FInput,
-      FButton,
-      File,
-      ProgressBar
-    },
-    methods:{
-      createImage(){
+      vm.source = files;
+    });
+  },
+  methods: {
+    createImage() {
+      if (
+        !FormValidator(this.$data, {
+          name: {
+            required: true,
+            pattern: "^[A-Za-z0-9-_]+$",
+            name: "نام ایمیج"
+          }
+        })
+      )
+        return;
+      this.loading = true;
+      this.$store
+        .dispatch("createImage", { name: this.name })
+        .then(res => {
+          this.$notify({
+            title: res.message,
+            time: 3000,
+            type: "success"
+          });
 
-        if(!FormValidator(this.$data, {name: {required: true, pattern: '^[A-Za-z0-9-_]+$', name: 'نام ایمیج'}})) return
-
-        this.loading = true
-        this.$store.dispatch('createImage', {name: this.name}).then(res => {
-
-            this.$notify({
-              title: res.message,
-              time: 3000,
-              type: 'success'
-            })
-
-          if(this.version.length){
-            this.loadingProgress = true
+          if (this.version.length && this.source) {
+            this.loadingProgress = true;
 
             let fd = formData([
               {
-                name: 'version',
+                name: "version",
                 value: this.version
               },
               {
-                type: 'image',
-                name: 'source'
+                name: "source",
+                value: this.source
               }
-            ])
+            ]);
 
-            this.$store.dispatch('createImageVersion', {name: this.name, formData: fd}).then(res => {
-              this.$notify({
-                title: res.message,
-                time: 4000,
-                type: 'success'
-              })
-              this.loading = false
-              this.loadingProgress = false
-              this.$router.push(`/dashboard/images/${this.name}/versions/${this.version}/logs`)
-            }).catch(e => {
-              this.loading = false
-              this.loadingProgress = false
-              ErrorReporter(e, this.$data, true).forEach(error => {
+            this.$store
+              .dispatch("createImageVersion", { name: this.name, formData: fd })
+              .then(res => {
                 this.$notify({
-                  title: error,
+                  title: res.message,
                   time: 4000,
-                  type: 'error'
-                })
+                  type: "success"
+                });
+                this.loading = false;
+                this.loadingProgress = false;
+                this.$router.push(
+                  `/dashboard/images/${this.name}/versions/${this.version}/logs`
+                );
               })
-            })
+              .catch(e => {
+                this.loading = false;
+                this.loadingProgress = false;
+                ErrorReporter(e, this.$data, true).forEach(error => {
+                  this.$notify({
+                    title: error,
+                    time: 4000,
+                    type: "error"
+                  });
+                });
+              });
           } else {
-            this.loading = false
-            this.$router.push('/dashboard/images')
+            this.loading = false;
+            this.$router.push("/dashboard/images");
           }
-        }).catch(e => {
-          this.loading = false
+        })
+        .catch(e => {
+          this.loading = false;
           ErrorReporter(e, this.$data, true).forEach(error => {
             this.$notify({
               title: error,
               time: 4000,
-              type: 'error'
-            })
-          })
-        })
-      }
+              type: "error"
+            });
+          });
+        });
     }
   }
+};
 </script>
