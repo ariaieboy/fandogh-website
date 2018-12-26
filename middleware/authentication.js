417dc7013a48ec58947a47a64014080e77338f2d
davodaslanifakor <davodaslanifakor@gmail.com>
2018-12-26 14:18:22 +0330

    test ssr req =

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,15 +1,19 @@
-import {getToken} from "../utils/cookie";
+import { getToken } from "../utils/cookie";
 
-export default async function ({req, isServer, redirect, route}){
-  
-  if(!route.path.includes('dashboard')) return
-  if(isServer){
-    if(!req.cookies['USER_TOKEN']){
-      redirect('/user/login')
-    }
-  } else {
-    if(!getToken()){
-      redirect('/user/login')
+export default async function({ req, isServer, redirect, route }) {
+  if (!route.path.includes("dashboard") || !route.path.startsWith("/dashboard")) {
+    if (!getToken()) {
+      redirect("/user/login");
     }
   }
+
+  // if(isServer){
+  //   if(!req.cookies['USER_TOKEN']){
+  //     redirect('/user/login')
+  //   }
+  // } else {
+  //   if(!getToken()){
+  //     redirect('/user/login')
+  //   }
+  // }
 }
