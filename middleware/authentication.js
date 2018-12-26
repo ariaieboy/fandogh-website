5f260b682684fc05ce382e8d82395f6cf291312f
davodaslanifakor <davodaslanifakor@gmail.com>
2018-12-26 14:24:49 +0330

    edit b for test

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,7 +1,7 @@
 import { getToken } from "../utils/cookie";
 
 export default async function({ req, isServer, redirect, route }) {
-  if (!route.path.includes("dashboard") || !route.path.startsWith("/dashboard")) {
+  if (route.path.includes("dashboard") || route.path.startsWith("/dashboard")) {
     if (!getToken()) {
       redirect("/user/login");
     }
