8e586b7b311520b9d720fb733975367e7a84114b
davodaslanifakor <davodaslanifakor@gmail.com>
2018-12-26 13:43:51 +0330

    add raychatScript for dashbord

--- layouts/dashboard.vue
+++ layouts/dashboard.vue
@@ -65,7 +65,12 @@ export default {
         this.$router.push("/user/login");
       }
     }
-  }
+  },
+  mounted(){
+    const raychatScript = document.createElement('script')
+    raychatScript.innerText = '!function(){function t(){var t=document.createElement("script");t.type="text/javascript",t.async=!0,localStorage.getItem("rayToken")?t.src="https://app.raychat.io/scripts/js/"+o+"?rid="+localStorage.getItem("rayToken")+"&href="+window.location.href:t.src="https://app.raychat.io/scripts/js/"+o;var e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(t,e)}var e=document,a=window,o="b34779ab-3e49-4256-8f71-ec8ae7e76d64";"complete"==e.readyState?t():a.attachEvent?a.attachEvent("onload",t):a.addEventListener("load",t,!1)}();'
+    document.head.appendChild(raychatScript)
+    },
 };
 </script>
 
