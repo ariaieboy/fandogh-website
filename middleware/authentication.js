1b4e203dd27cb2aa69fd65e427d20d3f387c6c32
davodaslanifakor <davodaslanifakor@gmail.com>
2018-12-26 14:33:24 +0330

    authentication contition

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,12 +1,12 @@
-import {getToken} from "../utils/cookie";
+import { getToken } from "../utils/cookie";
 
-export default async function ({req, isServer, redirect, route}){
-  
-  if(!route.path.includes('dashboard') || !route.path.startsWith('/dashboard')) return
-  console.log(isServer)
-  if(!getToken()){
-    redirect('/user/login')
+export default async function({ req, isServer, redirect, route }) {
+  if (route.path.includes("dashboard") || route.path.startsWith("/dashboard")) {
+    if (!getToken()) {
+      redirect("/user/login");
+    }
   }
+
   // if(isServer){
   //   if(!req.cookies['USER_TOKEN']){
   //     redirect('/user/login')
