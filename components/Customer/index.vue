d488c20c51f7c213cd72db2078bf42e79091e33b
davodaslanifakor <davodaslanifakor@gmail.com>
2019-01-13 11:38:58 +0330

    update methods handel slider

--- components/Customer/index.vue
+++ components/Customer/index.vue
@@ -5,19 +5,22 @@
         <h2 class="title">فندقی‌ها</h2>
         <p class="description">برخی از مشتری‌های فندق</p>
       </div>
-      <div class="customer-wrapper">
+      <div class="customer-wrapper" @mouseover="stopSlider" @mouseout="startSlider">
         <div class="customer-item-wrapper">
-          <div class="customer-items" v-for="item in items">
-            <div class="customer-item" v-for="image in item" @click="setActive(image)">
+          <div class="customer-items" v-for="(item,j) in items">
+            <div class="customer-item" v-for="(image,index) in item" @click="setActive(image,index,j)" :class="{active:isActive === index && parentActive === j}">
               <img :src="`${image.logo}`" alt>
               <a :href="image.link">{{image.text}}</a>
             </div>
           </div>
         </div>
         <div class="customer-banner">
-          <div class="customer-banner-item">
-            <!-- <img :src="activeImage" alt> -->
-            <span :style="{'background-image':'url('+activeImage+')'}"></span>
+          <div   v-for="(item,j) in items">
+            <div  v-for="(image,index) in item"  v-if="isActive === index && parentActive === j">
+              <div class="customer-banner-item">
+                <span :style="{'background-image':'url('+image.cover+')'}"></span>
+              </div>
+          </div>
           </div>
         </div>
       </div>
@@ -31,7 +34,11 @@ export default {
 
   data() {
     return {
+      isActive:0,
+      parentActive:0,
       activeImage: "img/customer/cover-roozame.png",
+      interval:null,
+      activeCount : 1,
       items: [
         [
           {
@@ -60,17 +67,58 @@ export default {
           {
             logo: "img/customer/logo-pay-ping-small.png",
             id: 4,
-            cover: "img/customer/cover-payping.jpg",
+            cover: "img/customer/cover-payping.png",
             text: "payping.ir",
             link: "https://www.payping.ir"
           }
         ]
-      ]
+      ],
     };
   },
+  mounted(){
+    this.startSlider()
+  },
+  destroyed() {
+    this.stopSlider()
+  },
   methods: {
-    setActive(item) {
+    setActive(item,index,j) {
       this.activeImage = item.cover;
+      this.isActive = index
+      this.parentActive = j
+       if(this.isActive === 0 && this.parentActive === 0) {
+             this.activeCount = 1
+        }
+        if(this.isActive === 1 && this.parentActive === 0) {
+             this.activeCount = 2
+        }
+        if(this.isActive === 0 && this.parentActive === 1) {
+             this.activeCount = 3
+        }
+        if(this.isActive === 1 && this.parentActive === 1) {
+             this.activeCount = 0
+        }
+    },
+    startSlider(){
+      this.interval = setInterval(() => {
+        this.activeCount +=1
+        if(this.activeCount === 1) {
+          this.setActive(this.items[0][0],0,0)
+        }
+        if(this.activeCount === 2) {
+          this.setActive(this.items[1][0],1,0)
+        }
+        if(this.activeCount === 3) {
+          this.setActive(this.items[0][1],0,1)
+        }
+        if(this.activeCount === 4) {
+          this.setActive(this.items[1][1],1,1)
+          this.activeCount = 0
+        }
+      }, 5000);
+    },
+    stopSlider(){
+      clearInterval(this.interval)
     }
   }
 };
@@ -99,7 +147,12 @@ export default {
     min-width 270px
     min-height 180px
     border-radius 10px
+    position: relative;
     background-color rgba(0, 0, 0, 0.14)
+    transition: box-shadow 0.3s ease-in-out;
+    &.active
+      box-shadow: 0px 0px 2px 2px rgba(38,198,236,1);
+      
     a
       margin-top 20px
       color #fff
