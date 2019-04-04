fc315de255275684b08b163fc72e699ac4622523
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2019-04-04 22:42:32 +0430

    margin-bottom added to profile-plan root node

--- components/Dashboard/profile/profile-plan.vue
+++ components/Dashboard/profile/profile-plan.vue
@@ -1,6 +1,6 @@
 <template>
 
-    <div>
+    <div style="margin-bottom: 50px">
         <div class="row plan-container" style="padding: 12px">
 
             <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
