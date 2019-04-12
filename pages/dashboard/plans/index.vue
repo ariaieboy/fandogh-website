c5a32171774d68fa582ec02ea37d9c56af6898f1
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2019-04-12 15:36:31 +0430

    some commented codes reverted.

--- pages/dashboard/plans/index.vue
+++ pages/dashboard/plans/index.vue
@@ -388,9 +388,9 @@
                     this.finalBill.dedicatedVolume = this.planData.dedicatedVolume;
                 }
                 if (this.quota !== null) {
-                    // if (this.quota.memory_limit / 1024 >= 1) {
-                    //     this.finalBill.memory += this.quota.memory_limit / 1024;
-                    // }
+                    if (this.quota.memory_limit / 1024 >= 1) {
+                        this.finalBill.memory += this.quota.memory_limit / 1024;
+                    }
 
                     if (this.quota.volume_limit > 0) {
                         this.finalBill.dedicatedVolume += this.quota.volume_limit;
