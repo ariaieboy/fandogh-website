bdeed94d6fb469526b33103a4217fcdaf15b1fc3
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2019-04-13 21:30:19 +0430

    extra headers deleted

--- components/Dashboard/header/index.vue
+++ components/Dashboard/header/index.vue
@@ -25,9 +25,6 @@ export default {
     return {
       list: [
         { icon: 'Group 2188', text: 'حساب کاربری', link: '/dashboard/account' },
-        { icon: 'Group 2190', text: 'پلن من', link: '/dashboard/plans' },
-        { icon: 'Group 2188', text: 'کیف پول', link: '/dashboard/wallet' },
-        { icon: 'ic_settings', text: 'تنظیمات', link: '/dashboard/account/edit' },
       ]
     }
   },
