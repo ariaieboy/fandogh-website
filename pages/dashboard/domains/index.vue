e04c2ae3354b62a156cd013c84d8caeea10b4527
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2019-05-08 18:06:04 +0430

    develop domain ssl icon test

--- pages/dashboard/domains/index.vue
+++ pages/dashboard/domains/index.vue
@@ -153,19 +153,19 @@
                 return verified ? "تایید شده" : "تایید نشده";
             },
             getStatus(certificate) {
-                if (!certificate) return '<div style="display: flex"><img style="margin-left: auto; margin-right: auto;" class="ssl" src="../../../components/Dashboard/home/icons/ssl-error.svg" alt="ssl-status"></div>';
-                if (!certificate.details) return '<div style="display: flex"><img style="margin-left: auto; margin-right: auto;" class="ssl" src="../../../components/Dashboard/home/icons/ssl-error.svg" alt="ssl-status"></div>';
+                if (!certificate) return `<div style="display: flex"><img style="margin-left: auto; margin-right: auto;" class="ssl" :src="require('./icons/ssl-error.svg')" alt="ssl-status"></div>`;
+                if (!certificate.details) return '<div style="display: flex"><img style="margin-left: auto; margin-right: auto;" class="ssl" src="./icons/ssl-error.svg" alt="ssl-status"></div>';
                 const {status} = certificate.details;
                 if (!status) return "";
                 let value = status.toLowerCase();
                 if (value === "ready") {
-                    return '<div style="display: flex"><img style="margin-left: auto; margin-right: auto;" class="ssl" src="../../../components/Dashboard/home/icons/ssl-success.svg" alt="ssl-status"></div>';
+                    return '<div style="display: flex"><img style="margin-left: auto; margin-right: auto;" class="ssl" src="./icons/ssl-success.svg" alt="ssl-status"></div>';
                 }
                 if (value === "error") {
-                    return '<div style="display: flex"><img style="margin-left: auto; margin-right: auto;" class="ssl" src="../../../components/Dashboard/home/icons/ssl-error.svg" alt="ssl-status"></div>';
+                    return '<div style="display: flex"><img style="margin-left: auto; margin-right: auto;" class="ssl" src="./icons/ssl-error.svg" alt="ssl-status"></div>';
                 }
                 if (value === "unknown") {
-                    return '<div style="display: flex"><img style="margin-left: auto; margin-right: auto;" class="ssl" src="../../../components/Dashboard/home/icons/ssl-error.svg" alt="ssl-status"></div>';
+                    return '<div style="display: flex"><img style="margin-left: auto; margin-right: auto;" class="ssl" src="./icons/ssl-error.svg" alt="ssl-status"></div>';
                 }
             },
             verify(index) {
