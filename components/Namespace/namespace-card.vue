99ecc7f6153d5bc8491df5850698e17d27c9e5d7
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2019-05-23 06:22:44 +0430

    develop namespace card style changed to tag style

--- components/Namespace/namespace-card.vue
+++ components/Namespace/namespace-card.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12" style="padding: 0">
         <div class="box middle-xs">
-            <div style="cursor: pointer; flex: 0.2 0 auto">
+            <div style="flex: 0.2 0 auto;">
                 <!--<img style="width: 1.4em; height: 1.4em; vertical-align: middle; justify-self: right"-->
                      <!--alt="delete-namespace"-->
                      <!--src="../../static/icons/ic_delete.svg"/>-->
-                <span :style="{color: namespace.quota.memory_limit/1024 >=1 ? '#2979ff' : '#6A6A6A'}">{{(namespace.quota.memory_limit/1024 >=1 ? 'حرفه‌ای' : 'رایگان')}}</span>
+                <span style="border-left: 1px solid #838383; padding-left: 12px;" :style="{color: namespace.quota.memory_limit/1024 >=1 ? '#2979ff' : '#6A6A6A'}">{{(namespace.quota.memory_limit/1024 >=1 ? 'حرفه‌ای' : 'رایگان')}}</span>
             </div>
 
             <div class="text-container">
@@ -28,14 +28,14 @@
     .box
         display flex
         margin .5rem 0.5rem
-        height 62px
+        height 58px
         min-height 32px
-        border-radius 3px
+        border-radius 50px
         background-color #fefefe
         text-align center
         padding-left 16px
         padding-right 16px
-        box-shadow 0 2px 6px rgba(0, 0, 0, 0.09)
+        box-shadow 0 2px 6px rgba(0,0,0,0.03)
 
     div.text-container
         margin 0 16px 0 0
