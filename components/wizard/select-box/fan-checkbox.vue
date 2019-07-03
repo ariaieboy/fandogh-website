f0c7213fcb40f49dc87db4a75ec9de2549de919f
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2019-07-03 16:05:04 +0430

    feature/wizard-new-face fan-checkbox component created. [a custom check box for wizard]

---
+++ components/wizard/select-box/fan-checkbox.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="fan-checkbox-container" :class="[object.selected ? 'selected': '']">
+
+        <span>{{object.label}}</span>
+
+        <div class="circle-border"><div class="circle-fill"></div></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "fan-checkbox",
+        props:['object']
+    }
+</script>
+
+<style lang="stylus" scoped>
+
+    .fan-checkbox-container
+        width max-content
+        margin-left 16px
+        height 30px
+        border-radius 50px
+        border 1px solid #7c7c7c
+        font-size .9em
+        font-family: iran-yekan
+        box-sizing padding-box
+        padding 0 12px
+        display inline-flex
+        cursor pointer
+
+
+        span
+            min-width 75px
+            vertical-align middle
+            text-align center
+            line-height 30px
+            color #7c7c7c
+            font-family: iran-yekan
+            @media only screen and (max-width: 500px)
+                min-width 50px
+
+        div.circle-border
+            width 20px
+            height 20px
+            padding 1px
+            border-radius 50%
+            border 1px solid #7c7c7c
+            margin auto 12px auto -6px
+            box-sizing padding-box
+
+        div.circle-fill
+            width 16px
+            height 16px
+            background-color transparent
+            border-radius 50%
+
+        &.selected
+            border-color: #0093ff
+            span
+                color: #0093ff
+
+            div.circle-border
+                border-color: #0093ff
+
+            div.circle-fill
+                background-color #0093ff
+
+        &:hover
+            border-color: #0093ff
+            transition all .3s ease-in-out
+            span
+                color: #0093ff
+                transition all .3s ease-in-out
+
+            div.circle-border
+                border-color: #0093ff
+                transition all .3s ease-in-out
+
+            div.circle-fill
+                background-color #0093ff
+                transition all .3s ease-in-out
+
+
+
+
+
+
+
+</style>
