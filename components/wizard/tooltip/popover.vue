4d7a82426c1346f15f9f3eec520b480bc998ea86
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2019-07-03 16:05:16 +0430

    feature/wizard-new-face popover component created.

---
+++ components/wizard/tooltip/popover.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-popover offset="16" :disabled="!isEnabled"
+               style="align-self: baseline; float: left; margin-top: 6px; margin-left: 0">
+        <img @click="isEnabled = !isEnabled" src="../../../assets/svg/ic_info.svg" alt="tooltip"
+             style="cursor: pointer; width: 16px; margin-bottom: -23px; z-index: 1000; position: relative;
+                                        filter: invert(27%) sepia(27%) saturate(7260%) hue-rotate(143deg) brightness(105%) contrast(108%)"/>
+
+        <template slot="popover">
+            <p class="tooltip-content"
+               style="font-family: iran-yekan;font-weight: bold; font-size: 1.2em; color:#222222; margin-bottom: 7px">{{tooltip.title}}</p>
+            <div style="height: 1px; background-color: #00e5ff; margin-bottom: 7px"></div>
+
+            <p style="font-size: 1em; font-family: iran-yekan; line-height: 1.7; color: #333333">
+                {{tooltip.text}}</p>
+
+            <div style="width: 100%; display: flex; justify-items: center; justify-content: center">
+                <a :href="tooltip.url"
+                   target="_blank"
+                   style=" cursor: pointer; background-color: #2979ff;box-shadow: 0 0 6px rgba(41,121, 255, 0.7), 0 0 8px rgba(41,121, 255, 0.4); font-weight: bold; padding: 7px 12px; border-radius: 3px">مطالعه‌ی مستندات</a>
+                <a v-close-popover
+                   style=" cursor: pointer;margin-right: 12px; border: 1px solid #00e5ff; font-weight: bold; padding: 7px 12px; border-radius: 3px; color: #00e5ff">بستن</a>
+            </div>
+        </template>
+
+    </v-popover>
+</template>
+
+<script>
+    export default {
+        name: "popover",
+        props:['tooltip'],
+        data(){
+            return{
+                isEnabled: false,
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
