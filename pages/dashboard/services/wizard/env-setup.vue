2c96c1648ea3cdbfd15726fbece790b4bc179fde
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2019-07-05 00:01:38 +0430

    feature/wizard-new-face create_env section object created.

--- pages/dashboard/services/wizard/env-setup.vue
+++ pages/dashboard/services/wizard/env-setup.vue
@@ -4,6 +4,22 @@
 
         <banner :page="page"></banner>
 
+        <div style="margin-top: 12px">
+
+
+            <config-box :section-title="sections.create_env" :tooltip="sections.create_env_tooltip">
+
+                <div style="display: flex; flex-wrap: wrap; justify-content: center">
+
+
+                </div>
+
+
+            </config-box>
+
+
+        </div>
+
 
     </div>
     
@@ -34,6 +50,14 @@
                         '                می‌ماند در ذهن‌هابرای انتحا توه سرویس برای آنکه این متن بک تست بمانید سمنیا در دست داشتن است برای فندق\n' +
                         '                که می‌ماند در ذهن‌ها'
                 },
+                sections:{
+                    create_env: 'ساخت Environment Variable',
+                    create_env_tooltip: {
+                        title: 'ساخت Environment Variable',
+                        text: 'برای آنکه بتوانید از Environment Variableها استفاده نمایید، باید آن‌ها را در مانیفست خود اضافه نمایید.',
+                        url: 'https://docs.fandogh.cloud/docs/service-manifest.html#env'
+                    },
+                }
 
             }
         },
