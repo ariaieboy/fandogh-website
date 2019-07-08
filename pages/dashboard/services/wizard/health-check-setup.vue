7015635d1834ca220fb65f7070560b6ea06d8b87
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2019-07-08 11:28:30 +0430

    feature/wizard-new-face lifecycle methods added.

--- pages/dashboard/services/wizard/health-check-setup.vue
+++ pages/dashboard/services/wizard/health-check-setup.vue
@@ -1,15 +1,32 @@
 <template>
-<div>
+    <div>
 
-</div>
+    </div>
 </template>
 
 <script>
+
+    import Banner from "../../../../components/wizard/banner/banner";
+    import Popover from "../../../../components/wizard/tooltip/popover";
+    import ConfigBox from "../../../../components/wizard/box/config-box";
+
     export default {
-        name: "health-check-setup"
+        name: "health-check-setup",
+        components: {
+            Banner,
+            Popover,
+            ConfigBox
+        },
+        data() {
+            return {}
+        },
+        methods: {},
+        computed: {}, watch: {}, created() {
+        }, mounted() {
+        }
     }
 </script>
 
-<style scoped>
+<style lang="stylus" scoped>
 
 </style>
