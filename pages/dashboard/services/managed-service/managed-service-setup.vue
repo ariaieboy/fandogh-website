47697bf24e4a0d1deafedb5a3e55c78f7c76eced
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2019-07-19 19:44:45 +0430

    feature/wizard-managed-service managed-service-setup component created.

---
+++ pages/dashboard/services/managed-service/managed-service-setup.vue
@@ -0,0 +1,19 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "managed-service-setup",
+        props:[],
+        data(){
+            return{
+
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
