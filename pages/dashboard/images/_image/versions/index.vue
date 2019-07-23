9b7b479dc8c5a5e92f546c6f9e431ed49203591d
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2019-07-23 19:49:16 +0430

    feature/create-domain-new-design create domain style changed. image version title bug fixed.

--- pages/dashboard/images/_image/versions/index.vue
+++ pages/dashboard/images/_image/versions/index.vue
@@ -9,7 +9,7 @@
         </f-empty>
         <div class="images" v-else>
             <div style="overflow: hidden; margin-bottom: 32px">
-                <div class="right" style="float: right;"><p class="title"> لیست ورژن‌های
+                <div class="right" style="float: right;"><p class="title_header"> لیست ورژن‌های
                     {{image}}</p></div>
                 <div class="left" style="float: left; cursor: pointer; margin-top: 8px"
                      @click="$router.push(`/dashboard/images/${image}/versions/create`)">
@@ -167,7 +167,7 @@
     };
 </script>
 <style lang="stylus" scoped>
-    .title
+    .title_header
         font-family iran-yekan
         font-style normal
         font-weight bold
