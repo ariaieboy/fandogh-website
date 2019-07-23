35e892c86ade2020d26ab63ad2ad4cd87f4bd076
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2019-07-24 01:20:11 +0430

    Develop file button color set to black

--- components/elements/file/index.js
+++ components/elements/file/index.js
@@ -12,7 +12,7 @@ export default {
     return <div class="upload-btn-wrapper">
       <label> فایل خود را اینجا آپلود کنید</label>
       <input  accept=".zip"  onChange={e => rename(e.target.files[0].name)} type="file" id={props.id} />
-      <button id={'file'} ref={'file'} style={'border-radius: 25px; padding: 12px 32px; font-family: iran-yekan; border: 1px solid #00E5FF; max-width: 200px; overflow: hidden; text-overflow: ellipsis;'}> Browse </button>
+      <button id={'file'} ref={'file'} style={'border-radius: 25px; padding: 12px 32px; font-family: iran-yekan; border: 1px solid #00E5FF; max-width: 200px; overflow: hidden; text-overflow: ellipsis;color: #1d1d1d;'}> Browse </button>
     </div>
   }
 }
