01df18ea2bffb95900d2da2159655d2e4cd01826
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2019-08-07 17:58:44 +0430

    develop audience component completed.

--- components/Landing/sections/audiences.vue
+++ components/Landing/sections/audiences.vue
@@ -1,46 +1,28 @@
 <template>
-    <div style="width: 100%; min-height: 600px; position: relative; margin-top: -48px;">
+    <div class="audience-container">
         <img src="../../../assets/svg/pod_bg.svg" width="100%" style="opacity: 20%;position: absolute"/>
-        <div style="width: 100%; background-image: linear-gradient(to bottom, rgba(254,254,254,0.75), #fefefe 47%, #fefefe);position: relative; display: flex; flex-direction: column; padding-top: 250px">
-
-            <p style="margin-left: auto; margin-right: auto; font-family: iran-yekan;font-size: 35px;font-weight: bold;font-style: normal;font-stretch: normal;line-height: 1.71;letter-spacing: normal;text-align: left;color: #0045ff;">
-                فندق برای چه کسانی است؟
-            </p>
-
-            <div style="width: 350px; height: 0; border: solid 2px #0045ff; margin-left: auto; margin-right: auto; border-radius: 25px; margin-top: 16px"></div>
 
 
-            <div style="width: 100%; margin-top: 80px; min-height: 120px; display: flex; justify-content: center;">
+        <div class="audience-inner-container">
 
-                <div style="display: flex; flex-direction: column; justify-content: center; cursor: pointer">
-                    <img src="../../../assets/svg/targets/innovators.svg"
-                         style="margin-left: 24px; margin-right: 24px">
-                    <p style="  font-family: iran-yekan; font-size: 18px;font-weight: normal;font-style: normal;font-stretch: normal;line-height: 1.72;letter-spacing: normal;text-align: center;color: #2979ff; margin-top: 12px; ">
-                        نو‌آوران</p>
-                </div>
+            <p class="aud-title">فندق برای چه کسانی است؟</p>
 
-                <div style="display: flex; flex-direction: column; justify-content: center; cursor: pointer">
-                    <img src="../../../assets/svg/targets/teams.svg" style="margin-left: 24px; margin-right: 24px">
-                    <p style="  font-family: iran-yekan; font-size: 18px;font-weight: normal;font-style: normal;font-stretch: normal;line-height: 1.72;letter-spacing: normal;text-align: center;color: #2979ff; margin-top: 12px; ">
-                        تیم‌ها</p>
-                </div>
+            <div class="horizontal-line"></div>
 
-                <div style="display: flex; flex-direction: column; justify-content: center; cursor: pointer">
-                    <img src="../../../assets/svg/targets/startups.svg" style="margin-left: 24px; margin-right: 24px">
-                    <p style="  font-family: iran-yekan; font-size: 18px;font-weight: normal;font-style: normal;font-stretch: normal;line-height: 1.72;letter-spacing: normal;text-align: center;color: #2979ff; margin-top: 12px; ">
-                        استارتاپ‌ها</p>
-                </div>
 
-                <div style="display: flex; flex-direction: column; justify-content: center; cursor: pointer">
-                    <img src="../../../assets/svg/targets/developers.svg"
-                         style="margin-left: 24px; margin-right: 24px">
-                    <p style="  font-family: iran-yekan; font-size: 18px;font-weight: normal;font-style: normal;font-stretch: normal;line-height: 1.72;letter-spacing: normal;text-align: center;color: #2979ff; margin-top: 12px; ">
-                        برنامه‌نویسان</p>
+            <div class="audience-rail row">
+
+                <div v-for="item in audiences" class="aud-item">
+                    <img :src="require('../../../assets/svg/targets/' + item.image + '.svg')" :alt="item.image">
+                    <div style="width: 100%; padding-left: 12px; padding-right: 12px; display: flex; flex-direction: column">
+                        <p>{{item.title}}</p>
+                        <div class="divider"></div>
+                    </div>
                 </div>
 
             </div>
 
-            <div style="  font-family: iran-yekan;font-size: 18px;font-weight: normal;font-style: normal;font-stretch: normal;line-height: 1.72;letter-spacing: normal;margin-left: auto;margin-right: auto;text-align: center;color: #3c3c3c; max-width: 600px; margin-top: 64px">
+            <div class="aud-description">
                 مهم نیست که در حال توسعه یک محصول اولیه هستید یا صاحب یک کسب‌وکار بزرگ، سکوی ابری فندق برای هر
                 زمان
                 از نیاز‌های شما، یک راه حل دارد.
@@ -52,10 +34,158 @@
 
 <script>
     export default {
-        name: "audiences"
+        name: "audiences",
+        data() {
+            return {
+                audiences: [
+                    {
+                        image: 'innovators',
+                        title: 'نو‌آوران',
+                        href: ''
+                    },
+                    {
+                        image: 'teams',
+                        title: 'تیم‌ها',
+                        href: ''
+                    },
+                    {
+                        image: 'startups',
+                        title: 'استارتاپ‌ها',
+                        href: ''
+                    },
+                    {
+                        image: 'developers',
+                        title: 'برنامه‌نویسان',
+                        href: ''
+                    }
+                ]
+            }
+        }
     }
 </script>
 
 <style lang="stylus" scoped>
+
+    .audience-container
+        width 100%
+        min-height 600px
+        position relative
+        margin-top -48px
+
+
+    .audience-inner-container
+        width 100%
+        background-image linear-gradient(to bottom, rgba(254, 254, 254, 0.75), #fefefe 47%, #fefefe)
+        position relative
+        display flex
+        flex-direction column
+        padding-top 250px
+        @media only screen and (max-width: 992px)
+            padding-top 180px
+
+        p.aud-title
+            margin-left auto
+            margin-right auto
+            font-family iran-yekan
+            font-size 2.4em
+            font-weight bold
+            font-style normal
+            font-stretch normal
+            line-height 1.71
+            letter-spacing normal
+            text-align left
+            color #0045ff
+            @media only screen and (max-width: 992px)
+                font-size 1.8em
+
+
+        div.horizontal-line
+            width 450px
+            height 0
+            border solid 2px #0045ff
+            margin-left auto
+            margin-right auto
+            border-radius 25px
+            margin-top 8px
+            @media only screen and (max-width: 992px)
+                width 350px
+
+    .audience-rail
+        width 100%
+        margin-top 80px
+        display flex
+        margin-left 0
+        margin-right 0
+        height max-content
+        justify-content center
+
+        div.aud-item
+            display flex
+            height max-content
+            flex-direction column
+            justify-content center
+            cursor pointer
+            @media only screen and (max-width: 992px)
+                margin-top 16px
+
+            img
+                margin-left 24px
+                margin-right 24px
+
+            div
+                p
+                    font-family iran-yekan
+                    font-size 1.3em
+                    font-weight normal
+                    font-style normal
+                    font-stretch normal
+                    line-height 1.72
+                    letter-spacing normal
+                    text-align center
+                    margin-bottom 5px
+                    color #2979ff
+                    margin-top 12px
+                    @media only screen and (max-width: 992px)
+                        font-size 1.1em
+
+
+                div.divider
+                    width available
+                    height 0
+                    border-radius 25px
+                    margin-top 0
+                    display none
+                    border 1px solid #0045ff
+
+        div.aud-item:hover
+            div
+                p
+                    color #0045ff
+                    font-weight bold
+                div.divider
+                    display unset
+
+    .aud-description
+        font-family iran-yekan
+        font-size 1.4em
+        font-weight normal
+        font-style normal
+        font-stretch normal
+        line-height 1.72
+        letter-spacing normal
+        margin-left auto
+        margin-right auto
+        text-align center
+        color #3c3c3c
+        max-width 700px
+        margin-top 64px
+        margin-bottom 32px
+        padding-right 16px
+        padding-left 16px
+        @media only screen and (max-width: 992px)
+            font-size 1.1em
+            margin-top 32px
+
+
 
 </style>
