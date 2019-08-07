45c1fcf0c2876e5700ab08abc28a4bafa8755d32
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2019-08-07 17:21:06 +0430

    develop pod_bg svg changed. audience component created.

---
+++ components/Landing/sections/audiences.vue
@@ -0,0 +1,61 @@
+<template>
+    <div style="width: 100%; min-height: 600px; position: relative; margin-top: -48px;">
+        <img src="../../../assets/svg/pod_bg.svg" width="100%" style="opacity: 20%;position: absolute"/>
+        <div style="width: 100%; background-image: linear-gradient(to bottom, rgba(254,254,254,0.75), #fefefe 47%, #fefefe);position: relative; display: flex; flex-direction: column; padding-top: 250px">
+
+            <p style="margin-left: auto; margin-right: auto; font-family: iran-yekan;font-size: 35px;font-weight: bold;font-style: normal;font-stretch: normal;line-height: 1.71;letter-spacing: normal;text-align: left;color: #0045ff;">
+                فندق برای چه کسانی است؟
+            </p>
+
+            <div style="width: 350px; height: 0; border: solid 2px #0045ff; margin-left: auto; margin-right: auto; border-radius: 25px; margin-top: 16px"></div>
+
+
+            <div style="width: 100%; margin-top: 80px; min-height: 120px; display: flex; justify-content: center;">
+
+                <div style="display: flex; flex-direction: column; justify-content: center; cursor: pointer">
+                    <img src="../../../assets/svg/targets/innovators.svg"
+                         style="margin-left: 24px; margin-right: 24px">
+                    <p style="  font-family: iran-yekan; font-size: 18px;font-weight: normal;font-style: normal;font-stretch: normal;line-height: 1.72;letter-spacing: normal;text-align: center;color: #2979ff; margin-top: 12px; ">
+                        نو‌آوران</p>
+                </div>
+
+                <div style="display: flex; flex-direction: column; justify-content: center; cursor: pointer">
+                    <img src="../../../assets/svg/targets/teams.svg" style="margin-left: 24px; margin-right: 24px">
+                    <p style="  font-family: iran-yekan; font-size: 18px;font-weight: normal;font-style: normal;font-stretch: normal;line-height: 1.72;letter-spacing: normal;text-align: center;color: #2979ff; margin-top: 12px; ">
+                        تیم‌ها</p>
+                </div>
+
+                <div style="display: flex; flex-direction: column; justify-content: center; cursor: pointer">
+                    <img src="../../../assets/svg/targets/startups.svg" style="margin-left: 24px; margin-right: 24px">
+                    <p style="  font-family: iran-yekan; font-size: 18px;font-weight: normal;font-style: normal;font-stretch: normal;line-height: 1.72;letter-spacing: normal;text-align: center;color: #2979ff; margin-top: 12px; ">
+                        استارتاپ‌ها</p>
+                </div>
+
+                <div style="display: flex; flex-direction: column; justify-content: center; cursor: pointer">
+                    <img src="../../../assets/svg/targets/developers.svg"
+                         style="margin-left: 24px; margin-right: 24px">
+                    <p style="  font-family: iran-yekan; font-size: 18px;font-weight: normal;font-style: normal;font-stretch: normal;line-height: 1.72;letter-spacing: normal;text-align: center;color: #2979ff; margin-top: 12px; ">
+                        برنامه‌نویسان</p>
+                </div>
+
+            </div>
+
+            <div style="  font-family: iran-yekan;font-size: 18px;font-weight: normal;font-style: normal;font-stretch: normal;line-height: 1.72;letter-spacing: normal;margin-left: auto;margin-right: auto;text-align: center;color: #3c3c3c; max-width: 600px; margin-top: 64px">
+                مهم نیست که در حال توسعه یک محصول اولیه هستید یا صاحب یک کسب‌وکار بزرگ، سکوی ابری فندق برای هر
+                زمان
+                از نیاز‌های شما، یک راه حل دارد.
+            </div>
+        </div>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "audiences"
+    }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -4,58 +4,8 @@
         <div class="row" style="width: 100%">
 
             <slider/>
-
-
             <languages/>
-
-            <!--<div style="width: 100%; min-height: 600px; position: relative; margin-top: -48px;">-->
-                <!--<img src="../assets/svg/pod_bg.svg" width="100%" style="opacity: 20%;position: absolute"/>-->
-                <!--<div style="width: 100%; background-image: linear-gradient(to bottom, rgba(254,254,254,0.75), #fefefe 47%, #fefefe);position: relative; display: flex; flex-direction: column; padding-top: 250px">-->
-
-                    <!--<p style="margin-left: auto; margin-right: auto; font-family: iran-yekan;font-size: 35px;font-weight: bold;font-style: normal;font-stretch: normal;line-height: 1.71;letter-spacing: normal;text-align: left;color: #0045ff;">-->
-                        <!--فندق برای چه کسانی است؟-->
-                    <!--</p>-->
-
-                    <!--<div style="width: 350px; height: 0; border: solid 2px #0045ff; margin-left: auto; margin-right: auto; border-radius: 25px; margin-top: 16px"></div>-->
-
-
-                    <!--<div style="width: 100%; margin-top: 80px; min-height: 120px; display: flex; justify-content: center;">-->
-
-                        <!--<div style="display: flex; flex-direction: column; justify-content: center; cursor: pointer">-->
-                            <!--<img src="../assets/svg/targets/innovators.svg"-->
-                                 <!--style="margin-left: 24px; margin-right: 24px">-->
-                            <!--<p style="  font-family: iran-yekan; font-size: 18px;font-weight: normal;font-style: normal;font-stretch: normal;line-height: 1.72;letter-spacing: normal;text-align: center;color: #2979ff; margin-top: 12px; ">-->
-                                <!--نو‌آوران</p>-->
-                        <!--</div>-->
-
-                        <!--<div style="display: flex; flex-direction: column; justify-content: center; cursor: pointer">-->
-                            <!--<img src="../assets/svg/targets/teams.svg" style="margin-left: 24px; margin-right: 24px">-->
-                            <!--<p style="  font-family: iran-yekan; font-size: 18px;font-weight: normal;font-style: normal;font-stretch: normal;line-height: 1.72;letter-spacing: normal;text-align: center;color: #2979ff; margin-top: 12px; ">-->
-                                <!--تیم‌ها</p>-->
-                        <!--</div>-->
-
-                        <!--<div style="display: flex; flex-direction: column; justify-content: center; cursor: pointer">-->
-                            <!--<img src="../assets/svg/targets/startups.svg" style="margin-left: 24px; margin-right: 24px">-->
-                            <!--<p style="  font-family: iran-yekan; font-size: 18px;font-weight: normal;font-style: normal;font-stretch: normal;line-height: 1.72;letter-spacing: normal;text-align: center;color: #2979ff; margin-top: 12px; ">-->
-                                <!--استارتاپ‌ها</p>-->
-                        <!--</div>-->
-
-                        <!--<div style="display: flex; flex-direction: column; justify-content: center; cursor: pointer">-->
-                            <!--<img src="../assets/svg/targets/developers.svg"-->
-                                 <!--style="margin-left: 24px; margin-right: 24px">-->
-                            <!--<p style="  font-family: iran-yekan; font-size: 18px;font-weight: normal;font-style: normal;font-stretch: normal;line-height: 1.72;letter-spacing: normal;text-align: center;color: #2979ff; margin-top: 12px; ">-->
-                                <!--برنامه‌نویسان</p>-->
-                        <!--</div>-->
-
-                    <!--</div>-->
-
-                    <!--<div style="  font-family: iran-yekan;font-size: 18px;font-weight: normal;font-style: normal;font-stretch: normal;line-height: 1.72;letter-spacing: normal;margin-left: auto;margin-right: auto;text-align: center;color: #3c3c3c; max-width: 600px; margin-top: 64px">-->
-                        <!--مهم نیست که در حال توسعه یک محصول اولیه هستید یا صاحب یک کسب‌وکار بزرگ، سکوی ابری فندق برای هر-->
-                        <!--زمان-->
-                        <!--از نیاز‌های شما، یک راه حل دارد.-->
-                    <!--</div>-->
-                <!--</div>-->
-            <!--</div>-->
+            <audiences/>
 
             <!--<div style="width: 100%; min-height: 300px; background: #0045ff; margin-top: 32px; display: flex; flex-direction: column; position:relative;">-->
 
@@ -827,6 +777,7 @@
     import FCustomer from '~/components/Customer'
     import Slider from "../components/Landing/sections/slider";
     import Languages from "../components/Landing/sections/languages";
+    import Audiences from "../components/Landing/sections/audiences";
 
     export default {
         components: {
@@ -838,7 +789,8 @@
             Solution,
             Tutorial,
             Prices,
-            Languages
+            Languages,
+            Audiences
         },
         data() {
             return {}
