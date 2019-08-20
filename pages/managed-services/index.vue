d656690d51db7f7be5d561ba439881ac199d0c55
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2019-08-20 22:43:58 +0430

    feature/manage-service-index managed-service changed to managed-services. data models created.

---
+++ pages/managed-services/index.vue
@@ -0,0 +1,34 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "index",
+        data(){
+            return{
+                title: '',
+                description: '',
+                features:[],
+                managed_services:{
+                    title: '',
+                    description: '',
+                    services:[]
+                },
+                tutorial:{
+                    title: '',
+                    description: '',
+                    video: ''
+                },
+                register: {
+                    title: ''
+                }
+
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
