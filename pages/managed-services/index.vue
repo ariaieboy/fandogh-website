4da50bfe622702ad90ad18a78826dde5fd6bf237
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2019-08-20 23:30:45 +0430

    feature/manage-service-index managed service feature icons added. managed service feature section completed.

--- pages/managed-services/index.vue
+++ pages/managed-services/index.vue
@@ -12,8 +12,24 @@
             <h2>{{feature_model.title}}</h2>
             <h4>{{feature_model.description}}</h4>
 
-        </div>
+            <div class="row" style="width: 100%; margin-top: 48px; max-width: 1370px; margin-left: auto; margin-right: auto">
 
+                <div v-for="feature in feature_model.features" class="managed-service-feature-card col-xs-12 col-sm-6 col-md-4 col-lg-4">
+                    <span>
+                        <img :src="require('./img/icons/' + feature.icon)" :alt="feature.icon">
+                    </span>
+                    <div>
+                        <h5>{{feature.title}}</h5>
+                        <p>{{feature.description}}</p>
+                    </div>
+
+                </div>
+
+                <div style="  height: 0; border: solid .5px #c4c2c2;"></div>
+
+            </div>
+
+        </div>
 
 
     </div>
@@ -23,24 +39,55 @@
 <script>
     export default {
         name: "index",
-        data(){
-            return{
+        data() {
+            return {
                 title: 'سرویس‌های مدیریت شده',
                 description: 'فقط چند کلیک فاصله میان شما و محبوب‌ترین و پر‌ استفاده‌ترین تکنولوژی‌ها وجود دارد',
-                feature_model:{
+                feature_model: {
                     title: 'قدرتمند، ایمن و مقیاس‌پذیر',
                     description: 'به راحتی سرویس مورد نیاز خود را بسازید و دیگر نگران مقیاس‌پذیری و تنظیمات‌ پیچیده و پشتیبانی آن نباشید',
                     features: [
 
+                        {
+                            title: 'گستره سرویس‌های ارائه شده',
+                            description: 'تمام سرویس‌های محبوب و پر استفاده را در یک پکیج با گستره بالا برای شما فراهم آورده‌ایم تا سرعت توسعه محصولات شما را افزایش دهیم.',
+                            icon: 'choice_range.svg'
+                        },
+                        {
+                            title: 'مقیاس‌پذیری بالا',
+                            description: 'سرویس‌های مدیریت شده به راحتی و با کمترین هزینه به هر میزان که نیاز داشته باشید تغییر مقیاس می‌دهند تا در هر لحظه پاسخگوی نیازهای‌ شما باشند.',
+                            icon: 'ic-auto-scaling.svg'
+                        },
+                        {
+                            title: 'نصب سریع',
+                            description: 'شما تنها با چند کلیک ساده می‌توانید هر کدام از سرویس‌های مدیریت شده و قدرتمند سکوی ابری فندق را در فضانام خود داشته باشید.',
+                            icon: 'ic-service.svg'
+                        },
+                        {
+                            title: 'امنیت بالا',
+                            description: 'سرویس‌های مدیریت شده از طریق Private Network فضانام شما با دیگر سرویس‌ها در تماس هستند و از بیرون هیچگونه دسترسی به آن‌ها وجود ندارد.',
+                            icon: 'ic-secret.svg'
+                        },
+                        {
+                            title: 'پشتیبانی و توسعه',
+                            description: 'سرویس‌های مدیریت شده توسط تیم سکوی ابری فندق توسعه یافته و نگهداری می‌شوند؛ در نتیجه نگرانی بابت توسعه و نگهداری آن‌ها برای کاربران وجود ندارد.',
+                            icon: 'code.png'
+                        },
+                        {
+                            title: 'قابل اعتماد',
+                            description: 'سرویس‌های مدیریت شده از روی نسخه‌های stable ساخته می‌شوند و نگرانی بابت مشکلات نسخه‌های فرعی یا بتا و یا احتمال failure در آن‌ها وجود ندارد.',
+                            icon: 'reliability.svg'
+                        },
+
                     ]
 
                 },
-                managed_services:{
+                managed_services: {
                     title: '',
                     description: '',
-                    services:[]
+                    services: []
                 },
-                tutorial:{
+                tutorial: {
                     title: '',
                     description: '',
                     video: ''
@@ -116,28 +163,76 @@
         background #fefefe
         padding 32px 24px
         width 100%
+
         h2
             font-family iran-yekan
-            font-size  2.2em
-            font-weight  normal
-            font-style  normal
-            font-stretch  normal
-            line-height  1.73
-            letter-spacing  normal
-            text-align  center
-            color  #3c3c3c
+            font-size 2.2em
+            font-weight normal
+            font-style normal
+            font-stretch normal
+            line-height 1.73
+            letter-spacing normal
+            text-align center
+            color #3c3c3c
+            @media only screen and (max-width 992px)
+                font-size 1.8em
+
         h4
-            font-family  iran-yekan
-            font-size  1.4em
-            font-weight  normal
-            font-style  normal
-            font-stretch  normal
-            line-height  1.71
+            font-family iran-yekan
+            font-size 1.4em
+            font-weight normal
+            font-style normal
+            font-stretch normal
+            line-height 1.71
             margin-top 16px
-            letter-spacing  normal
-            text-align  center
+            letter-spacing normal
+            text-align center
             padding-left 10%
             padding-right 10%
-            color  #535353
+            color #535353
+            @media only screen and (max-width 992px)
+                font-size 1.2em
+
+    .managed-service-feature-card
+        display flex
+        width 100%
+        margin-top 32px
+        margin-bottom 32px
+        span
+            width 52px
+            height 52px
+            border-radius 5px
+            background-color rgba(0, 69, 255, 0.2)
+            display flex
+            img
+                width 36px
+                height 36px
+                margin auto
+        div
+            display flex
+            flex-direction column
+            flex 1
+            padding-right 16px
+            h5
+                font-family: iran-yekan;
+                font-size: 1.4em;
+                font-weight: normal;
+                font-style: normal;
+                font-stretch: normal;
+                line-height: 1.75;
+                letter-spacing: normal;
+                text-align: right;
+                color: #3c3c3c;
+            p
+                font-family: iran-yekan;
+                font-size: 1.2em;
+                font-weight: normal;
+                font-style: normal;
+                font-stretch: normal;
+                line-height: 1.75;
+                letter-spacing: normal;
+                margin-top 8px
+                text-align: right;
+                color: #535353;
 
 </style>
