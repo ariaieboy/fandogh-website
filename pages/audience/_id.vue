f363165eb798e75bc3da25535e41a3fe8ce491f0
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2019-08-21 21:44:33 +0430

    feature/audience-section audience_model data created.

--- pages/audience/_id.vue
+++ pages/audience/_id.vue
@@ -4,7 +4,49 @@
 
 <script>
     export default {
-        name: "_id"
+        name: "_id",
+        data(){
+            return{
+
+                audience_model:{
+                    students: {
+                        title: 'دانشجویان',
+                        description: 'برای دانشجویان با ایده‌های ناب و بودجه محدود',
+                        icon: ''
+                    },
+                    startups: {
+                        title: 'استارتاپ‌ها',
+                        description: 'سکویی برای توسعه، مدیریت و مقیاس‌پذیری پروژه‌های شما',
+                        icon: ''
+                    },
+                    innovators: {
+                        title: 'نوآوران',
+                        description: 'ایده خود را به سرعت تبدیل به محصول کنید و در اختیار میلیون‌ها کاربر بگذارید',
+                        icon: ''
+                    },
+                    teams: {
+                        title: 'تیم‌ها',
+                        description: 'سهولت در توسعه و سادگی در مدیریت',
+                        icon: ''
+                    },
+                    companies: {
+                        title: 'شرکت‌ها',
+                        description: 'راه‌حل‌های Enterprise در برابر هزینه‌های پایین و مزایای بی‌شمار',
+                        icon: ''
+                    },
+                    freelancers: {
+                        title: 'Freelancers',
+                        description: 'برای Freelancer هایی که نیاز به میزبانی یکپارچه و تطبیق‌پذیر دارند',
+                        icon: ''
+                    },
+                    developers: {
+                        title: 'برنامه‌نویسان',
+                        description: 'هزینه کم، قابلیت‌های نامحدود و هزینه پایین',
+                        icon: ''
+                    },
+                }
+            }
+        }
     }
 </script>
 
