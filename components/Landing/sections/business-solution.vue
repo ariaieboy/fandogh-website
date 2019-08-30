00821aad6498d78f2f12c1edae5367075281d051
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2019-08-30 17:53:05 +0430

    develop - travis commit.

--- components/Landing/sections/business-solution.vue
+++ components/Landing/sections/business-solution.vue
@@ -31,7 +31,6 @@
             </div>
         </div>
 
-
         <div style="width: 100%; display: flex">
             <a href="mailto:support@fandogh.cloud" class="business-solution-button">درخواست مشاوره</a>
         </div>
