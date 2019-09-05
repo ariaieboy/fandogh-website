6f5313b93a4052ef5bb945c54bd54f6668d18a24
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2019-09-05 23:12:08 +0430

    develop - member-invite-container node and class created.

--- components/Dashboard/profile/team-management.vue
+++ components/Dashboard/profile/team-management.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="row" style="margin-right: 0; margin-left: 0;width: 100%; height: max-content">
-        <div>
+        <div class="member-invite-container">
 
         </div>
 
@@ -15,5 +15,14 @@
 </script>
 
 <style lang="stylus" scoped>
+
+    @import "../../../assets/css/variables.styl"
+
+    .member-invite-container
+        display flex
+        flex-direction row
+        background $totalWhite
+        border-radius 5px
+        box-shadow 0 2px 6px rgba(0, 0, 0, 0.17)
 
 </style>
