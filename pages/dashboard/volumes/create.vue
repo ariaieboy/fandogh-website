8093519e08dc9125cd0ea5c4cc6c73540348a6b3
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2019-09-28 19:48:36 +0330

    develop - activePlan added to localStorage. - usage chart added.

--- pages/dashboard/volumes/create.vue
+++ pages/dashboard/volumes/create.vue
@@ -1,56 +1,73 @@
 <template>
     <div>
         <h2 class="title_header">ساخت فضای ذخیره‌سازی</h2>
+
+
         <div style="background: #fefefe; border-radius: 3px; box-shadow: 0 2px 6px rgba(0,0,0, 0.17); padding: 16px">
-            <div class="row">
+            <div class="row" style="display: flex; width: 100%">
 
-                <div style="display: flex; margin-bottom: 24px" class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                <div style="display: flex; margin-bottom: 24px"
+                     class="col-xs-12 col-sm-12 col-md-6 col-lg-6 volume-chart">
+                    <f-charts
+                            class="pr-lg-25"
+                            title="فضای ذخیره‌سازی"
+                            :data="volumeUsage"
+                            unit="گیگابایت"
+                            :colors="['#086375','#3ccc38']"/>
+                </div>
 
-                    <v-text-field style="font-family: iran-yekan; font-size: 1em;margin-left: -15px"
-                                  color="#0045ff"
-                                  type="text"
-                                  dir="ltr"
-                                  required
-                                  :rules="[rules.counter, rules.required, rules.volume_name_regex]"
-                                  :counter="volume.name.counter"
-                                  :maxlength="volume.name.counter"
-                                  v-model="volume.name.value"
-                                  :hint="volume.name.hint"
-                                  :label="volume.name.label">
+                <div style="display: flex; margin-bottom: 24px; flex-direction: column"
+                     class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
 
-                    </v-text-field>
+                    <div style="display: flex; margin-bottom: 24px; width: 100%">
 
-                    <popover :tooltip="tooltips.name"></popover>
+                        <v-text-field style="font-family: iran-yekan; font-size: 1em;margin-left: -15px"
+                                      color="#0045ff"
+                                      type="text"
+                                      dir="ltr"
+                                      required
+                                      :rules="[rules.counter, rules.required, rules.volume_name_regex]"
+                                      :counter="volume.name.counter"
+                                      :maxlength="volume.name.counter"
+                                      v-model="volume.name.value"
+                                      :hint="volume.name.hint"
+                                      :label="volume.name.label">
+
+                        </v-text-field>
+
+                        <popover :tooltip="tooltips.name"></popover>
+
+                    </div>
+
+                    <div style="display: flex; margin-bottom: 24px; width: 100%">
+
+                        <v-text-field style="font-family: iran-yekan; font-size: 1em;margin-left: -15px"
+                                      color="#0045ff"
+                                      type="number"
+                                      dir="ltr"
+                                      :min="10"
+                                      required
+                                      :rules="[rules.required, rules.min_value]"
+                                      v-model.number="volume.capacity.value"
+                                      :hint="volume.capacity.hint"
+                                      :maxlength="volume.capacity.counter"
+                                      :label="volume.capacity.label">
+
+                        </v-text-field>
+
+                        <popover :tooltip="tooltips.capacity"></popover>
+
+                    </div>
 
                 </div>
 
-                <div style="display: flex; margin-bottom: 24px" class="col-xs-12 col-sm-12 col-md-6 col-lg-6">
+            </div>
 
-                    <v-text-field style="font-family: iran-yekan; font-size: 1em;margin-left: -15px"
-                                  color="#0045ff"
-                                  type="number"
-                                  dir="ltr"
-                                  :min="10"
-                                  required
-                                  :rules="[rules.required, rules.min_value]"
-                                  v-model.number="volume.capacity.value"
-                                  :hint="volume.capacity.hint"
-                                  :maxlength="volume.capacity.counter"
-                                  :label="volume.capacity.label">
-
-                    </v-text-field>
-
-                    <popover :tooltip="tooltips.capacity"></popover>
-
-                </div>
-
-                <div style="display: flex; width: 100%; padding: 32px 16px">
-                    <button style="background: #00E5FF; width: 100%; margin: auto; padding: 12px 0; border-radius: 3px; box-shadow: 0 2px 6px rgba(0, 229, 255, 0.4);
+            <div style="display: flex; width: 100%; padding: 32px 16px">
+                <button style="background: #00E5FF; width: 100%; margin: auto; padding: 12px 0; border-radius: 3px; box-shadow: 0 2px 6px rgba(0, 229, 255, 0.4);
                         max-width: 350px; outline: none; font-family: iran-yekan; color: #1d1d1d"
-                            @click="createVolume">ایجاد فضای ذخیره‌سازی جدید
-                    </button>
-                </div>
-
+                        @click="createVolume">ایجاد فضای ذخیره‌سازی جدید
+                </button>
             </div>
 
         </div>
@@ -63,12 +80,14 @@
     import Popover from "../../../components/wizard/tooltip/popover";
     import {getValue} from "../../../utils/cookie";
     import ErrorReporter from "../../../utils/ErrorReporter";
+    import FCharts from "~/components/Dashboard/home/usage.vue";
 
     export default {
         name: "create",
         layout: 'dashboard',
         components: {
             Popover,
+            FCharts,
         },
         data() {
             return {
@@ -113,6 +132,7 @@
             };
         },
         created() {
+
             this.$store.commit("SET_DATA", {data: false, id: "loading"});
 
         },
@@ -195,6 +215,15 @@
 
             }
 
+        },
+        computed: {
+            volumeUsage(){
+                let planObject = JSON.parse(localStorage.getItem('vuex')).activePlan;
+                const used = planObject.current_used_resources.volume_usage;
+                const free = planObject.quota.volume_limit - used;
+                const usage = [free, used];
+                return usage
+            }
         }
     }
 </script>
@@ -210,5 +239,10 @@
         line-height 1.75
         color #7c7c7c
         letter-spacing normal
+
+    .volume-chart
+        order 1
+        @media only screen and (max-width: 992px)
+            order 0
 
 </style>
