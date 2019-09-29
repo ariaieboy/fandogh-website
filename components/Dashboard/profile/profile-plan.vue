672ef0e048d1d84eab411740ca7ed9b13a0e3997
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2019-09-29 22:49:38 +0330

    feature/long-term-plan-order - profile-plan component edited. - month selection added to redeem plan.

--- components/Dashboard/profile/profile-plan.vue
+++ components/Dashboard/profile/profile-plan.vue
@@ -1,76 +1,87 @@
 <template>
 
-    <div style="margin-bottom: 50px">
-        <p class="date-tile">تاریخ پایان پلن شما:
-            <span class="date-value" v-if="planExpirationDate">{{planExpirationDate}}</span>
-            <span class="date-value" v-else>نامحدود</span>
-        </p>
-        <div class="row plan-container" style="padding: 12px;">
-
-            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12" style="height: 100px">
-                <div class="container-fluid row plan-item">
-                    <img class="plan-image" alt="cpu" src="../../../assets/svg/ic-cpu.svg">
-
-
-                    <div style="margin-top: auto;margin-bottom: auto;margin-right: 24px; padding: 0;">
-                        <p class="plan-title">پردازنده</p>
-                        <p class="plan-value">{{quota.cpu_limit}} هسته</p>
-
-                    </div>
-                </div>
+    <div style="margin-bottom: 50px; width: 100%">
+        <div class="row" style="width: 100%;">
+            <div style="width: 100%; display: flex">
+                <p class="date-tile">تاریخ پایان پلن شما:
+                    <span class="date-value" v-if="planExpirationDate">{{planExpirationDate}}</span>
+                    <span class="date-value" v-else>نامحدود</span>
+                </p>
             </div>
 
-            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12" style="height: 100px">
-                <div class="container-fluid row plan-item">
-                    <img alt="storage"
-                         class="plan-image"
-                         style="transform: scale(0.8)"
-                         src="../../../assets/svg/ic-storage.svg">
-                    </img>
-
-                    <div style="margin-top: auto;margin-bottom: auto;margin-right: 24px; padding: 0;">
-
-                        <p class="plan-title">حافظه ذخیره‌سازی</p>
-                        <p class="plan-value">{{quota.volume_limit}} گیگابایت</p>
-
-                    </div>
+            <div v-if="verifyUserAccess({ADMIN:'ADMIN'})"
+                 class="profile-plan-action-container">
+                <div class="redeem-plan-container">
+                    <input v-model.number="monthCount" @input="monthChanged" dir="rtl" type="number"
+                           placeholder="مدت زمان تمدید پلن بین ۱ تا ۲۴ ماه"
+                           style="flex: 0.9 0 auto; padding: 0 16px; border: none; outline: none;border-radius: 3px;font-family: iran-sans">
+                    <button style="color: #fefefe; flex: 0.15 0 auto; border: none; outline: none; background-color: #24D5D8;font-family: iran-yekan; border-top-left-radius: 3px; border-bottom-left-radius: 3px; cursor: pointer; font-size: 1.1em"
+                            @click="redeemPlan">
+                        تمدید پلن فعلی
+                    </button>
                 </div>
-            </div>
-
-
-            <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12" style="height: 100px">
-                <div class="container-fluid row plan-item">
-                    <img alt="memory" src="../../../assets/svg/ic-memory.svg" class="plan-image"
-                         style="transform: rotate(45deg) scale(1.1)">
-                    </img>
-
-                    <div style="margin-top: auto;margin-bottom: auto;margin-right: 24px; padding: 0;">
-
-                        <p class="plan-title">رم</p>
-                        <p class="plan-value">{{(quota.memory_limit > 511 ? parseFloat(Math.fround(quota.memory_limit / 1024).toExponential(1)) : quota.memory_limit)}}
-                            <span v-if="quota.memory_limit>=1024">گیگابایت</span>
-                            <span v-else>مگابایت</span>
-                        </p>
-
-                    </div>
-                </div>
-            </div>
-
-        </div>
-
-        <div v-if="verifyUserAccess({ADMIN:'ADMIN'})" class="row " style="width: 100%; margin-top: 40px; margin-right: 0; margin-left: 0">
-            <div class="container-fluid navigation-container">
                 <button class="navigation-button"
                         style="background-color: #0045ff; box-shadow: 0 2px 6px 0 rgba(0, 69, 255, 0.42)"
                         @click="navigateToPlanPage">
-                    ساخت پلن جدید
+                    افزایش منابع پلن
                 </button>
+            </div>
 
-                <button class="navigation-button"
-                        @click="redeemPlan"
-                        style="box-shadow: 0 2px 6px 0 rgba(126, 211, 33, 0.42); background-color: #7ed321;">
-                    تمدید پلن فعلی
-                </button>
+
+            <div style="width: 100%; margin-top: 16px; display: flex">
+                <div class="plan-container" style="padding: 12px; width: 100%;">
+
+                    <div style="height: 100px; flex: 1 0 auto">
+                        <div class="container-fluid row plan-item">
+                            <img class="plan-image" alt="cpu" src="../../../assets/svg/ic-cpu.svg">
+
+
+                            <div style="margin-top: auto;margin-bottom: auto;margin-right: 24px; padding: 0;">
+                                <p class="plan-title">پردازنده</p>
+                                <p class="plan-value">{{quota.cpu_limit}} هسته</p>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div style="height: 100px; flex: 1 0 auto">
+                        <div class="container-fluid row plan-item">
+                            <img alt="storage"
+                                 class="plan-image"
+                                 style="transform: scale(0.8)"
+                                 src="../../../assets/svg/ic-storage.svg">
+
+
+                            <div style="margin-top: auto;margin-bottom: auto;margin-right: 24px; padding: 0;">
+
+                                <p class="plan-title">حافظه ذخیره‌سازی</p>
+                                <p class="plan-value">{{quota.volume_limit}} گیگابایت</p>
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div style="height: 100px; flex: 1 0 auto">
+                        <div class="container-fluid row plan-item">
+                            <img alt="memory" src="../../../assets/svg/ic-memory.svg" class="plan-image"
+                                 style="transform: rotate(45deg) scale(1.1)">
+
+
+                            <div style="margin-top: auto;margin-bottom: auto;margin-right: 24px; padding: 0;">
+
+                                <p class="plan-title">رم</p>
+                                <p class="plan-value">{{(quota.memory_limit > 511 ?
+                                    parseFloat(Math.fround(quota.memory_limit /
+                                    1024).toExponential(1)) : quota.memory_limit)}}
+                                    <span v-if="quota.memory_limit>=1024">گیگابایت</span>
+                                    <span v-else>مگابایت</span>
+                                </p>
+
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
     </div>
@@ -93,108 +104,7 @@
                     memory: 0,
                     dedicatedVolume: 0,
                 },
-                selectedMonth:{
-                    title: '۳۰ روز',
-                    value: 1
-                },
-                monthList:[
-                    {
-                        title: '۳۰ روز',
-                        value: 1
-                    },
-                    {
-                        title: '۲ ماه',
-                        value: 2
-                    },
-                    {
-                        title: '۳ ماه',
-                        value: 3
-                    },
-                    {
-                        title: '۴ ماه',
-                        value: 4
-                    },
-                    {
-                        title: '۵ ماه',
-                        value: 5
-                    },
-                    {
-                        title: '۶ ماه',
-                        value: 6
-                    },
-                    {
-                        title: '۷ ماه',
-                        value: 7
-                    },
-                    {
-                        title: '۸ ماه',
-                        value: 8
-                    },
-                    {
-                        title: '۹ ماه',
-                        value: 9
-                    },
-                    {
-                        title: '۱۰ ماه',
-                        value: 10
-                    },
-                    {
-                        title: '۱۱ ماه',
-                        value: 11
-                    },
-                    {
-                        title: '۱ سال',
-                        value: 12
-                    },
-                    {
-                        title: '۱۳ ماه',
-                        value: 13
-                    },
-                    {
-                        title: '۱۴ ماه',
-                        value: 14
-                    },
-                    {
-                        title: '۱۵ ماه',
-                        value: 15
-                    },
-                    {
-                        title: '۱۶ ماه',
-                        value: 16
-                    },
-                    {
-                        title: '۱۷ ماه',
-                        value: 17
-                    },
-                    {
-                        title: '۱۸ ماه',
-                        value: 18
-                    },
-                    {
-                        title: '۱۹ ماه',
-                        value: 19
-                    },
-                    {
-                        title: '۲۰ ماه',
-                        value: 20
-                    },
-                    {
-                        title: '۲۱ ماه',
-                        value: 21
-                    },
-                    {
-                        title: '۲۲ ماه',
-                        value: 22
-                    },
-                    {
-                        title: '۲۳ ماه',
-                        value: 23
-                    },
-                    {
-                        title: '۲ سال',
-                        value: 24
-                    }
-                ],
+                monthCount: null,
             }
         },
         components: {
@@ -215,7 +125,15 @@
             }
         },
         methods: {
-            verifyUserAccess(permitted_roles){
+            monthChanged(month) {
+                this.monthCount = month.target.value;
+                if (this.monthCount > 24) {
+                    this.monthCount = 24;
+                } else if (this.monthCount < 1) {
+                    this.monthCount = 1
+                }
+            },
+            verifyUserAccess(permitted_roles) {
                 return RoleAccessHandler(permitted_roles)
             },
             async requestUserPlan() {
@@ -240,7 +158,7 @@
 
             }, async redeemPlan() {
 
-                if(parseFloat(Math.fround(this.quota.memory_limit / 1024).toExponential(1)) < 0.5){
+                if (parseFloat(Math.fround(this.quota.memory_limit / 1024).toExponential(1)) < 0.5) {
                     alert('اول پلن بخر بعدش تمدید کن عزیزم');
                     return;
                 }
@@ -271,11 +189,13 @@
                     this.finalBill.memory = parseFloat(Math.fround(this.quota.memory_limit / 1024).toExponential(1));
                     this.finalBill.dedicatedVolume += this.quota.volume_limit;
 
-                    if(this.finalBill.memory > 0)
+                    if (this.finalBill.memory > 0)
                         bill['memory'] = this.finalBill.memory
-                    if(this.finalBill.dedicatedVolume > 0)
+                    if (this.finalBill.dedicatedVolume > 0)
                         bill['dedicated_volume'] = this.finalBill.dedicatedVolume
                 }
+
+                bill['month_count'] = this.monthCount === null ? 1 : this.monthCount;
                 return bill;
             },
             navigateToPlanPage() {
@@ -291,10 +211,23 @@
 
 <style lang="stylus" scoped>
 
+    .row
+        margin-left 0 !important
+        margin-right 0 !important
+        @media only screen and (max-width 992px)
+            margin-left -1rem !important
+            margin-right -1rem !important
     .plan-container
         border-radius 3px
+        display flex
+        width 100%
         box-shadow 0 2px 6px 0 rgba(0, 0, 0, 0.07)
         background-color #ffffff
+        @media only screen and (max-width 992px)
+            flex-direction column
+            margin-right auto
+            margin-left auto
+            max-width 400px
 
     .plan-item
         width 100%
@@ -325,19 +258,17 @@
             font-size 1em
 
     .navigation-container
-        width min-content
+        width 100%
         margin-right auto
         margin-left auto
         display flex
         position relative
         justify-content center
-        @media only screen and (max-width: 700px)
-            display block
 
 
     .navigation-button
-        width 200px
-        height 40px
+        width 300px
+        height 50px
         background-color #0045ff
         outline none
         border none
@@ -346,13 +277,20 @@
         box-shadow 0 2px 6px 0 rgba(41, 121, 255, 0.42)
         color #ffffff
         font-family iran-yekan
-        font-size 0.9em
+        font-size 1em
         margin-left 5px
-        align-self center
-        margin-right 5px
+        margin-right auto
         cursor pointer
-        @media only screen and (max-width: 700px)
-            margin-top 12px
+        margin-top 16px
+        margin-bottom 24px
+        @media only screen and (max-width 992px)
+            margin-bottom 16px
+            max-width 400px
+            margin-top 0
+            width 100%
+            font-size 1.1em
+            margin-left auto
+
 
     .date-tile
         font-family yekan-bold
@@ -363,6 +301,10 @@
         font-stretch normal
         letter-spacing normal
         color: #7c7c7c
+        @media only screen and (max-width 992px)
+            margin-right auto
+            margin-left auto
+            font-size 1.5em
 
 
     .date-value
@@ -374,6 +316,29 @@
         font-stretch normal
         letter-spacing normal
         color: #222
+
+    .redeem-plan-container
+        border-radius 3px
+        box-shadow 0 2px 6px rgba(0, 0, 0, 0.09)
+        width 40%
+        height 50px
+        background-color #fefefe
+        margin-top 16px
+        margin-bottom 24px
+        display flex
+        padding 0
+        @media only screen and (max-width 992px)
+            margin-right auto
+            margin-left auto
+            max-width 400px
+            width 100%
+
+    .profile-plan-action-container
+        width: 100%;
+        display: flex
+        @media only screen and (max-width 992px)
+            flex-direction column
+
 
 
 </style>
