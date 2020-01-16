c73e33e3ea9892141f7e8a4938091b0e94fc663f
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2020-01-16 15:20:23 +0330

    feature/object-storage-managed-service - minio icon added. - minio card created.

--- pages/dashboard/services/setup_router.vue
+++ pages/dashboard/services/setup_router.vue
@@ -50,6 +50,7 @@
                     {title: 'MSSQL Server', description: 'Database', icon: 'mssql', path: "mssql"},
                     {title: 'MongoDB', description: 'Database', icon: 'mongodb', path: "mongodb"},
                     {title: 'Redis', description: 'In Memory Data Store', icon: 'redis', path: "redis"},
+                    {title: 'Minio', description: 'Object Storage', icon: 'minio', path: "minio"},
                     {title: 'Proxy', description: 'Proxy Service', icon: 'proxy', path: "proxy"}
                 ],
                 services: [
