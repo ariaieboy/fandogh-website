f87c14dbec4fec846920cd70f0fbe5105e7707aa
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2020-03-30 03:07:30 +0430

    enhancement/seo-enhancement - contact us page tags added.

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -7,7 +7,9 @@
                         ارتباط با ما
                     </h1>
                     <p>
-                        هدف ما در پروژه فندق، طراحی سکویی است برای تسهیل در استقرار و کاهش هزینه نگهداری سرویس‌های نرم‌افزاری که به کمک آن سرویس‌های ایرانی بتوانند تمرکز بیشتری روی کسب‌ و کار خود و نگرانی کمتری در مورد امور مربوط به استقرار، پایش، نگهداری و مقیاس‌پذیری داشته باشند.
+                        هدف ما در پروژه فندق، طراحی سکویی است برای تسهیل در استقرار و کاهش هزینه نگهداری سرویس‌های
+                        نرم‌افزاری که به کمک آن سرویس‌های ایرانی بتوانند تمرکز بیشتری روی کسب‌ و کار خود و نگرانی کمتری
+                        در مورد امور مربوط به استقرار، پایش، نگهداری و مقیاس‌پذیری داشته باشند.
                     </p>
 
                     <no-ssr>
@@ -33,7 +35,7 @@
                                 تلفن: ۰۲۱۲۸۴۲۲۳۸۳
                             </li>
                             <li>
-                                ایمیل:   support@fandogh.cloud
+                                ایمیل: support@fandogh.cloud
                             </li>
 
                         </ul>
@@ -45,196 +47,211 @@
 </template>
 
 <script>
-  import Single from '~/components/Single'
-  export default {
-    data () {
-      return {
-        mapStyle: [
-          {
-            'featureType': 'administrative',
-            'elementType': 'labels.text.fill',
-            'stylers': [
-              {
-                'color': '#ff9400'
-              }
-            ]
-          },
-          {
-            'featureType': 'administrative.province',
-            'elementType': 'all',
-            'stylers': [
-              {
-                'visibility': 'off'
-              }
-            ]
-          },
-          {
-            'featureType': 'landscape',
-            'elementType': 'all',
-            'stylers': [
-              {
-                'saturation': -100
-              },
-              {
-                'lightness': 65
-              },
-              {
-                'visibility': 'on'
-              }
-            ]
-          },
-          {
-            'featureType': 'poi',
-            'elementType': 'all',
-            'stylers': [
-              {
-                'saturation': -100
-              },
-              {
-                'lightness': 51
-              },
-              {
-                'visibility': 'simplified'
-              }
-            ]
-          },
-          {
-            'featureType': 'poi.park',
-            'elementType': 'geometry.fill',
-            'stylers': [
-              {
-                'lightness': '0'
-              },
-              {
-                'visibility': 'on'
-              },
-              {
-                'saturation': '12'
-              },
-              {
-                'color': '#adf4bb'
-              }
-            ]
-          },
-          {
-            'featureType': 'road.highway',
-            'elementType': 'all',
-            'stylers': [
-              {
-                'saturation': -100
-              },
-              {
-                'visibility': 'simplified'
-              }
-            ]
-          },
-          {
-            'featureType': 'road.arterial',
-            'elementType': 'all',
-            'stylers': [
-              {
-                'saturation': -100
-              },
-              {
-                'lightness': 30
-              },
-              {
-                'visibility': 'on'
-              }
-            ]
-          },
-          {
-            'featureType': 'road.arterial',
-            'elementType': 'labels.text.fill',
-            'stylers': [
-              {
-                'color': '#ff9400'
-              }
-            ]
-          },
-          {
-            'featureType': 'road.local',
-            'elementType': 'all',
-            'stylers': [
-              {
-                'saturation': -100
-              },
-              {
-                'lightness': 40
-              },
-              {
-                'visibility': 'on'
-              }
-            ]
-          },
-          {
-            'featureType': 'road.local',
-            'elementType': 'labels.text.fill',
-            'stylers': [
-              {
-                'color': '#7c7c7c'
-              }
-            ]
-          },
-          {
-            'featureType': 'transit',
-            'elementType': 'all',
-            'stylers': [
-              {
-                'saturation': -100
-              },
-              {
-                'visibility': 'simplified'
-              }
-            ]
-          },
-          {
-            'featureType': 'water',
-            'elementType': 'geometry',
-            'stylers': [
-              {
-                'hue': '#ffff00'
-              },
-              {
-                'lightness': -25
-              },
-              {
-                'saturation': -97
-              }
-            ]
-          },
-          {
-            'featureType': 'water',
-            'elementType': 'labels',
-            'stylers': [
-              {
-                'visibility': 'on'
-              },
-              {
-                'lightness': -25
-              },
-              {
-                'saturation': -100
-              }
-            ]
-          }
-        ]
-      }
-    },
-    components:{
-      Single
+    import Single from '~/components/Single'
+
+    export default {
+        data() {
+            return {
+                mapStyle: [
+                    {
+                        'featureType': 'administrative',
+                        'elementType': 'labels.text.fill',
+                        'stylers': [
+                            {
+                                'color': '#ff9400'
+                            }
+                        ]
+                    },
+                    {
+                        'featureType': 'administrative.province',
+                        'elementType': 'all',
+                        'stylers': [
+                            {
+                                'visibility': 'off'
+                            }
+                        ]
+                    },
+                    {
+                        'featureType': 'landscape',
+                        'elementType': 'all',
+                        'stylers': [
+                            {
+                                'saturation': -100
+                            },
+                            {
+                                'lightness': 65
+                            },
+                            {
+                                'visibility': 'on'
+                            }
+                        ]
+                    },
+                    {
+                        'featureType': 'poi',
+                        'elementType': 'all',
+                        'stylers': [
+                            {
+                                'saturation': -100
+                            },
+                            {
+                                'lightness': 51
+                            },
+                            {
+                                'visibility': 'simplified'
+                            }
+                        ]
+                    },
+                    {
+                        'featureType': 'poi.park',
+                        'elementType': 'geometry.fill',
+                        'stylers': [
+                            {
+                                'lightness': '0'
+                            },
+                            {
+                                'visibility': 'on'
+                            },
+                            {
+                                'saturation': '12'
+                            },
+                            {
+                                'color': '#adf4bb'
+                            }
+                        ]
+                    },
+                    {
+                        'featureType': 'road.highway',
+                        'elementType': 'all',
+                        'stylers': [
+                            {
+                                'saturation': -100
+                            },
+                            {
+                                'visibility': 'simplified'
+                            }
+                        ]
+                    },
+                    {
+                        'featureType': 'road.arterial',
+                        'elementType': 'all',
+                        'stylers': [
+                            {
+                                'saturation': -100
+                            },
+                            {
+                                'lightness': 30
+                            },
+                            {
+                                'visibility': 'on'
+                            }
+                        ]
+                    },
+                    {
+                        'featureType': 'road.arterial',
+                        'elementType': 'labels.text.fill',
+                        'stylers': [
+                            {
+                                'color': '#ff9400'
+                            }
+                        ]
+                    },
+                    {
+                        'featureType': 'road.local',
+                        'elementType': 'all',
+                        'stylers': [
+                            {
+                                'saturation': -100
+                            },
+                            {
+                                'lightness': 40
+                            },
+                            {
+                                'visibility': 'on'
+                            }
+                        ]
+                    },
+                    {
+                        'featureType': 'road.local',
+                        'elementType': 'labels.text.fill',
+                        'stylers': [
+                            {
+                                'color': '#7c7c7c'
+                            }
+                        ]
+                    },
+                    {
+                        'featureType': 'transit',
+                        'elementType': 'all',
+                        'stylers': [
+                            {
+                                'saturation': -100
+                            },
+                            {
+                                'visibility': 'simplified'
+                            }
+                        ]
+                    },
+                    {
+                        'featureType': 'water',
+                        'elementType': 'geometry',
+                        'stylers': [
+                            {
+                                'hue': '#ffff00'
+                            },
+                            {
+                                'lightness': -25
+                            },
+                            {
+                                'saturation': -97
+                            }
+                        ]
+                    },
+                    {
+                        'featureType': 'water',
+                        'elementType': 'labels',
+                        'stylers': [
+                            {
+                                'visibility': 'on'
+                            },
+                            {
+                                'lightness': -25
+                            },
+                            {
+                                'saturation': -100
+                            }
+                        ]
+                    }
+                ]
+            }
+        },
+        components: {
+            Single
+        },
+        head() {
+            return {
+                title: "ارتباط با ما",
+                meta: [
+                    {
+                        hid: 'keywords',
+                        name: 'keywords',
+                        content: 'سکو,فندق,تیم,توسعه,نرم افزار,سرویس,مقیاس‌پذیری,تهران'
+                    },
+                ]
+            };
+        }
     }
-  }
 </script>
 
 <style lang="stylus">
     .map
         height 250px
+
     .article
         ul
             li
                 color #000
                 padding 7px 0
+
                 &:first-child
                     margin-top 60px
 </style>
