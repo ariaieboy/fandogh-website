c6d0bc109d4f366683160738b7d3e294a8733ab8
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2020-03-29 22:18:51 +0430

    master - free/paid namespace recognition bug fixed. - free namespace tag color changed to green.

--- pages/dashboard/account/index.vue
+++ pages/dashboard/account/index.vue
@@ -67,7 +67,7 @@
                     <div style="min-height: 3em">
                         <p class="profile-entity-title">نوع کاربری:</p>
                         <p class="profile-entity-value" v-if="activeNamespace.quota">
-                            {{(activeNamespace.quota.memory_limit/1024 >=1 ? 'حرفه‌ای' : 'رایگان')}}</p>
+                            {{(activeNamespace.quota.memory_limit/512 >=1 ? 'حرفه‌ای' : 'رایگان')}}</p>
                     </div>
 
 
