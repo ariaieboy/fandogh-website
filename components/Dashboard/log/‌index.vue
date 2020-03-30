0bd36389296951eba71a566d7970112992831176
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2020-03-31 02:09:19 +0430

    enhancement/log-enhancement - index page title bug fixed.

--- components/Dashboard/log/‌index.vue
+++ components/Dashboard/log/‌index.vue
@@ -1,55 +1,91 @@
 <template>
-  <div v-if="logs" class="logs">
-    <table>
-      <tbody>
-        <tr v-for="(log, index) in logs">
-          <td>{{index+1}}</td>
-          <td> {{log}}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-  <f-loading v-else :isFull="true" />
+    <div v-if="logs && logs.length > 0" class="logs">
+        <table>
+            <tbody>
+            <tr v-for="(log, index) in logs">
+                <td>{{index+1}}</td>
+                <td v-html="processLogLine(log)"></td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+    <div class="logs" style="display: flex" v-else>
+        <p style="margin: auto; direction: rtl">در حال حاضر سرویس شما لاگ ندارد!</p>
+    </div>
 </template>
 
 <script>
-  import FLoading from "~/components/Loading";
-  export default {
-    props:['logs'],
-    components:{
-      FLoading
+    import FLoading from "~/components/Loading";
+
+    export default {
+        props: ['logs'],
+        components: {
+            FLoading
+        },
+        data() {
+            return {
+                time_stamps: null,
+                part_one: null
+            }
+        },
+        methods: {
+            processLogLine(line) {
+                let parts = line.toString().split('->');
+                let words = parts[0].match(/[+[a-zA-Z-0-9]+]/g);
+                var phrase = '';
+                for (let word of words) {
+                    phrase += `<span style="background-color: rgba(0,229,255,0.5); margin-right: 2px; padding-left: 2px"> ${word} </span> `
+                }
+                phrase += ' -> ';
+                parts[1] = parts[1].replace(/</gi, '&lt');
+                parts[1] = parts[1].replace(/>/gi, '&gt');
+                parts[1] = parts[1].replace(/&/gi, '&amp');
+                parts[1] = parts[1].replace(/&lt/gi, '');
+                parts[1] = parts[1].replace(/&gt/gi, '');
+                parts[1] = parts[1].replace(/&amp/gi, '');
+                let timestamps = parts[1].toString().match(/ [0-9.:T-]*Z/g);
+                if (timestamps) {
+                    for (let time of timestamps) {
+                        parts[1] = parts[1].replace(time, `<span style="background-color: rgba(79,204,84,0.5); margin-right: 2px; padding-left: 2px; padding-right: 2px;"> ${time} </span> `);
+                    }
+                }
+                phrase += parts[1];
+                return phrase;
+            }
+        }
     }
-  }
 </script>
 
 <style scoped lang="stylus">
-  .logs
-    width 100%
-    min-height 500px
-    max-height 1000px
-    overflow-y scroll
-    background #fff
-    direction ltr
-    padding 10px 0
-    border-radius 3px
-    box-shadow 0 2px 6px rgba(0,0,0, 0.07)
-    table
-      tr
-        td
-          width 100%
-          font-size 14px
-          height 25px
-          text-align left
-          padding 4px 16px 4px 0
-          font-family Roboto, sans-serif
-          &:first-child
-            min-width 60px
-            width 60px
-            text-align center
-            margin-right 5px
-        &:nth-child(even)
-          td
-            &:first-child
-              background-color #f2f2f2
+    .logs
+        width 100%
+        min-height 500px
+        height available
+        overflow-y scroll
+        background #fff
+        direction ltr
+        padding 10px 0
+        border-radius 3px
+        box-shadow 0 2px 6px rgba(0, 0, 0, 0.07)
+
+        table
+            tr
+                td
+                    width 100%
+                    font-size 14px
+                    height 25px
+                    text-align left
+                    padding 4px 4px 4px 0
+                    font-family Roboto, sans-serif
+
+                    &:first-child
+                        min-width 50px
+                        width max-content
+                        text-align center
+
+                &:nth-child(even)
+                    td
+                        &:first-child
+                            background-color #f2f2f2
 
 </style>
