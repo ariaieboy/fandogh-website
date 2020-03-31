ae73345c7557de11b91bf392790ef24294da4ffb
Sorena Sarabadani <sorena.sarabadani@gmail.com>
2020-04-01 01:14:29 +0430

    develop - fa lang added to header. - localhost redirection canceled.

--- redirect_to_www.js
+++ redirect_to_www.js
@@ -5,6 +5,9 @@ module.exports = function (req, res, next) {
     const env = process.env.NODE_ENV || 'staging';
     const canonicalDomain = 'www.fandogh.cloud';
 
+    if (host.toString().includes('localhost'))
+        return next();
+
     if (env === 'production' && host !== canonicalDomain && !host.toString().includes('fandogh-staging')) {
         res.writeHead(301, { Location: 'https://' + canonicalDomain + url });
         return res.end()
